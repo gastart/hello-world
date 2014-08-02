e8ea9780e36e0f7a51255e22ca0445a85fe87944
chesschi <chesschi@gmail.com>
2014-08-02 11:48:08 +0100

    Update maths.cpp
    Add subtract functions and implement minor changes.

--- maths.cpp
+++ maths.cpp
@@ -2,16 +2,30 @@
 
 int Maths::sum(const int a, const int b)
 {
-  int result = a + b;
-  return result;
+  return (a + b);
 }
 
-int sum(const std::vector<int> & v)
+int Maths::subtract(const int a, const int b)
+{
+  return (a - b);
+}
+
+int Maths::sum(const std::vector<int> & v)
 {
   int result = 0;
   for (std::vector<int>::size_type i = 0; i < v.size(); i++)
   {
-    result = sum(result, v[i]);
+    result += v[i];
+  }
+  return result;
+}
+
+int Maths::subtract(const int b, const std::vector<int> & v)
+{
+  int result = b;
+  for (std::vector<int>::size_type i = 0; i < v.size(); i++)
+  {
+    result -= v[i];
   }
   return result;
 }
