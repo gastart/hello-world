d1f485b11c306fda55a790c1b6383debd33eefbb
chesschi <chesschi@gmail.com>
2014-08-02 11:42:53 +0100

    Update maths.h
    Change to QAC compliance comments and add subtract functions

--- maths.h
+++ maths.h
@@ -6,15 +6,21 @@
 class Maths
 {
 public:
-  /**
-   * Sum two integers.
-   */
+  //!
+  //! Sum two integers.
   static int sum(const int a, const int b);
   
-  /**
-   * Sum a list of integers.
-   */
+  //!
+  //! Subtract two integers.
+  static int subtract(const int a, const int b);
+  
+  //!
+  //! Sum a list of integers.
   static int sum(const std::vector<int> & v);
+  
+  //!
+  //! Subtract a list of integers from b.
+  static int subtract(const int b, const std::vector<int> & v);
 };
 
 #endif // MATHS_H
